7759ef9c694724dfdf1e7126d08187713932db00
rl2k24 <00001107740253sp@al.educacao.sp.gov.br>
2024-11-24 22:56:37 +0000

    24/11

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Tecnologia',
+    'O que é Alura?',
+    'A Alura é uma escola online de tecnologia brasileira que oferece cursos, fórum e outras ferramentas para estudar e aprender sobre tecnologia'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Italia?',
+    'A capital da Espanha é Roma'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma div?',
+    'Div é uma tag do HTML que serve para agrupar blocos de elementos e aplicar estilos CSS a eles.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Oi em Inglês?',
+    'Oi em ingles é Hello! '
+)
